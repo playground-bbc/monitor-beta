a8881ae186b5f47bc1e17c360fc186b908901dbb
Eduardo Morales <spiderbbc@gmail.com>
2020-05-28 18:51:40 -0400

    Revert "Revert "Merge branch 'insights_series_products' into error_view_list""
    This reverts commit 6f2ae517d773fa40758a14f712ae635a0ed2c5ef.

--- models/WContent.php
+++ models/WContent.php
@@ -115,4 +115,12 @@ class WContent extends \yii\db\ActiveRecord
     {
         return $this->hasMany(WInsights::className(), ['content_id' => 'id']);
     }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getWProductsFamilyContent()
+    {
+        return $this->hasMany(WProductsFamilyContent::className(), ['contentId' => 'id']);
+    }
 }

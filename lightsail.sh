46fd13a53e1cb72025fd64efdfd2cb7cb3a57c47
Eduardo Morales <spiderbbc@gmail.com>
2020-08-31 16:55:20 -0400

    change script

--- lightsail.sh
+++ lightsail.sh
@@ -2,24 +2,28 @@
 
 # install latest version of docker the lazy way
 curl -sSL https://get.docker.com | sh
+# install composer
+sudo apt install composer
 
 # make it so you don't need to sudo to run docker commands
-usermod -aG docker ubuntu
+sudo usermod -aG docker ubuntu
 
 # install docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /home/ubuntu/docker-compose
+chmod +x /home/ubuntu/docker-compose
 
 # copy the dockerfile into /srv/docker 
 # if you change this, change the systemd service file to match
 # WorkingDirectory=[whatever you have below]
-mkdir /srv/docker
-curl -o /srv/docker/docker-compose.yml https://raw.githubusercontent.com/playground-bbc/monitor-beta/docker/docker-compose.yml
+mkdir /home/ubuntu/monitor
+cd /home/ubuntu/monitor
+git clone https://github.com/playground-bbc/monitor-beta.git .
 
+cd ..
 # copy in systemd unit file and register it so our compose file runs 
 # on system restart
-curl -o /etc/systemd/system/docker-compose-app.service https://raw.githubusercontent.com/mikegcoleman/todo/master/docker-compose-app.service
-systemctl enable docker-compose-app
+sudo curl -o /home/ubuntu/docker-compose-app.service https://raw.githubusercontent.com/mikegcoleman/todo/master/docker-compose-app.service
+sudo systemctl enable docker-compose-app
 
 # start up the application via docker-compose
-docker-compose -f /srv/docker/docker-compose.yml up -d
+./docker-compose -f /home/ubuntu/monitor/docker-compose.yml up -d

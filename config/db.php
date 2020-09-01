740b112659d52f38ec45fc11d82e5f1433f7d208
Eduardo Morales <spiderbbc@gmail.com>
2020-09-01 12:43:18 -0400

    change charset db file

--- config/db.php
+++ config/db.php
@@ -5,7 +5,7 @@ return [
     'dsn' => "mysql:host={$_SERVER['DB_HOST']};dbname={$_SERVER['DB_NAME']}",
     'username' => $_SERVER['DB_USER'],
     'password' => $_SERVER['DB_PASS'],
-    'charset' => 'utf8',
+    'charset' => 'utf8mb4',
     // Schema cache options (for production environment)
     'enableSchemaCache' => true,
     'schemaCacheDuration' => 60,

2baa28be4fb500bad6b577cc0d46f88222fcbaf9
Eduardo Morales <spiderbbc@gmail.com>
2020-11-23 18:45:00 -0300

    adding emails addres to email insigths

--- commands/EmailController.php
+++ commands/EmailController.php
@@ -79,9 +79,14 @@ class EmailController extends Controller
         }
         if(count($model)){
             $pathLogo = dirname(__DIR__)."/web/img/";
+            $userEmails = \app\models\Users::find()->select('email')->where(['status' => 10])->all();
+            $emails = [];
+            foreach ($userEmails as $userEmail) {
+                $emails[] = $userEmail->email;
+            }
             \Yii::$app->mailer->compose('insights',['model' => $model,'pathLogo' => $pathLogo])
             ->setFrom('monitormtg@gmail.com')
-            ->setTo(["spiderbbc@gmail.com"])->setSubject("Insigths de la Cuenta 📝: Mundo Lg")->send();
+            ->setTo($emails)->setSubject("Insigths de la Cuenta 📝: Mundo Lg")->send();
         }
 
         return ExitCode::OK;
